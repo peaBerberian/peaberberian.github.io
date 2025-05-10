b7b0898dfdf046bc7f7daa2fb0186cdad22e8936
Paul Berberian <paul.berberian@proton.me>
2025-05-10 17:39:47 +0200

    Deploy to the gh-pages

---
+++ lazy/example.js
@@ -0,0 +1,4 @@
+function e(){return{element:window.strHtml`<div class="w-content">
+${window.createAppTitle("Example application",{})}
+<p>${"I'm a lazy-loaded application, my code is not in the initial bundle, I'm only loaded when the application is first opened."}</p>
+</div>`}}export{e as create};

8e8f374fa1d2916583835f0602e32f0d0bb81e1d
Paul Berberian <paul.berberian@proton.me>
2025-05-21 00:00:30 +0200

    Add forgotten utils

--- apps/docx.mjs
+++ apps/docx.mjs
@@ -293,3 +293,17 @@ function handleForwardedEvent(iframe, eventData) {
     );
   }
 }
+
+/**
+ * Apply multiple style attributes on a given element.
+ * @param {HTMLElement} element - The `HTMLElement` on which the style should be
+ * aplied.
+ * @param {Object} style - The dictionnary where keys are style names (JSified,
+ * e.g. `backgroundColor` not `background-color`) and values are the
+ * corresponding syle values.
+ */
+export function applyStyle(element, style) {
+  for (const key of Object.keys(style)) {
+    element.style[key] = style[key];
+  }
+}
